7e6188046c3fbd27c4b8d3c0c2fe29164440ac01
VarshUI <145260709+VarshUI@users.noreply.github.com>
2025-07-09 22:35:42 +0200

    Add files via upload

---
+++ array_subset_of_another_array.js
@@ -0,0 +1,22 @@
+class Solution {
+    isSubset(a, b) {
+        const copyB = [...b];
+        const copyA = [...a];
+
+        for (let i = 0; i < copyB.length; i++) {
+            let found = false;
+
+            for (let j = 0; j < copyA.length; j++) {
+                if (copyB[i] === copyA[j]) {
+                    found = true;
+                    copyA.splice(j, 1); 
+                    break;
+                }
+            }
+
+            if (!found) return false;
+        }
+
+        return true;
+    }
+}
